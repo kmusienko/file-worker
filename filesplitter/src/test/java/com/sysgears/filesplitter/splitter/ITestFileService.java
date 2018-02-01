e05d08c943cb0f0657ca4e7989dc3594bc8653c9
konstantinmusienko <konstantin.musienko@sysgears.com>
2018-02-01 18:57:05 +0200

    Added unit tests for MergeCommandValidator.

--- filesplitter/src/test/java/com/sysgears/filesplitter/splitter/ITestFileService.java
+++ filesplitter/src/test/java/com/sysgears/filesplitter/splitter/ITestFileService.java
@@ -170,5 +170,4 @@ public class ITestFileService {
         //Act
         fileService.merge(command);
     }
-
 }
---
+++ filesplitter/src/test/java/com/sysgears/filesplitter/splitter/validator/UTestMergeCommandValidator.java
@@ -0,0 +1,36 @@
+package com.sysgears.filesplitter.splitter.validator;
+
+import com.sysgears.filesplitter.splitter.InvalidCommandException;
+import org.apache.log4j.Logger;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class UTestMergeCommandValidator {
+
+    private MergeCommandValidator mergeCommandValidator;
+
+    @BeforeClass
+    public void setUp() {
+        Logger logger = Logger.getRootLogger();
+        mergeCommandValidator = new MergeCommandValidatorImpl(logger);
+    }
+
+    @Test(expectedExceptions = InvalidCommandException.class)
+    public void testCheckCommandSignatureIfParametersAreWrong() throws InvalidCommandException {
+        //Arrange
+        String[] command = {"merge", "-way", "/home/konstantinmusienko/internship/parts"};
+
+        //Act
+        mergeCommandValidator.checkCommandSignature(command);
+    }
+
+    @Test
+    public void testCheckCommandSignatureIfCommandIsCorrect() throws InvalidCommandException {
+        //Arrange
+        String[] command = {"merge", "-p", "/home/konstantinmusienko/internship/parts"};
+
+        //Act
+        mergeCommandValidator.checkCommandSignature(command);
+    }
+
+}
